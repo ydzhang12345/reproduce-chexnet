b0b99fe11701a0c9aeef2e8cc3e64a25321b16e8
Ben <ydzhang12345@163.com>
2019-05-29 23:10:05 +0800

    add cnn visualization tutorials

---
+++ pytorch-cnn-visualizations/src/class_specific_samples.py
@@ -0,0 +1,221 @@
+# # Class visualization
+# By starting with a random noise image and performing gradient ascent on a target class, we can generate an image that the network will recognize as the target class. This idea was first presented in [2]; [3] extended this idea by suggesting several regularization techniques that can improve the quality of the generated image.
+# 
+# Concretely, let $I$ be an image and let $y$ be a target class. Let $s_y(I)$ be the score that a convolutional network assigns to the image $I$ for class $y$; note that these are raw unnormalized scores, not class probabilities. We wish to generate an image $I^*$ that achieves a high score for the class $y$ by solving the problem
+# 
+# $$
+# I^* = \arg\max_I s_y(I) - R(I)
+# $$
+# 
+# where $R$ is a (possibly implicit) regularizer (note the sign of $R(I)$ in the argmax: we want to minimize this regularization term). We can solve this optimization problem using gradient ascent, computing gradients with respect to the generated image. We will use (explicit) L2 regularization of the form
+# 
+# $$
+# R(I) = \lambda \|I\|_2^2
+# $$
+# 
+# **and** implicit regularization as suggested by [3] by periodically blurring the generated image. We can solve this problem using gradient ascent on the generated image.
+# 
+# In the cell below, complete the implementation of the `create_class_visualization` function.
+# 
+# [2] Karen Simonyan, Andrea Vedaldi, and Andrew Zisserman. "Deep Inside Convolutional Networks: Visualising
+# Image Classification Models and Saliency Maps", ICLR Workshop 2014.
+# 
+# [3] Yosinski et al, "Understanding Neural Networks Through Deep Visualization", ICML 2015 Deep Learning Workshop
+
+# In[74]:
+
+import torch
+from torch.autograd import Variable
+import torchvision
+import torchvision.transforms as T
+import random
+
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter1d
+import matplotlib.pyplot as plt
+
+
+SQUEEZENET_MEAN = np.array([0.485, 0.456, 0.406])
+SQUEEZENET_STD = np.array([0.229, 0.224, 0.225])
+def preprocess(img, size=224):
+    transform = T.Compose([
+        T.Scale(size),
+        T.ToTensor(),
+        T.Normalize(mean=SQUEEZENET_MEAN.tolist(),
+                    std=SQUEEZENET_STD.tolist()),
+        T.Lambda(lambda x: x[None]),
+    ])
+    return transform(img)
+
+def deprocess(img, should_rescale=True):
+    transform = T.Compose([
+        T.Lambda(lambda x: x[0]),
+        T.Normalize(mean=[0, 0, 0], std=(1.0 / SQUEEZENET_STD).tolist()),
+        T.Normalize(mean=(-SQUEEZENET_MEAN).tolist(), std=[1, 1, 1]),
+        T.Lambda(rescale) if should_rescale else T.Lambda(lambda x: x),
+        T.ToPILImage(),
+    ])
+    return transform(img)
+
+def rescale(x):
+    low, high = x.min(), x.max()
+    x_rescaled = (x - low) / (high - low)
+    return x_rescaled
+    
+def blur_image(X, sigma=1):
+    X_np = X.cpu().clone().numpy()
+    X_np = gaussian_filter1d(X_np, sigma, axis=2)
+    X_np = gaussian_filter1d(X_np, sigma, axis=3)
+    X.copy_(torch.Tensor(X_np).type_as(X))
+    return X
+
+def jitter(X, ox, oy):
+    """
+    Helper function to randomly jitter an image.
+    
+    Inputs
+    - X: PyTorch Tensor of shape (N, C, H, W)
+    - ox, oy: Integers giving number of pixels to jitter along W and H axes
+    
+    Returns: A new PyTorch Tensor of shape (N, C, H, W)
+    """
+    if ox != 0:
+        left = X[:, :, :, :-ox]
+        right = X[:, :, :, -ox:]
+        X = torch.cat([right, left], dim=3)
+    if oy != 0:
+        top = X[:, :, :-oy]
+        bottom = X[:, :, -oy:]
+        X = torch.cat([bottom, top], dim=2)
+    return X
+
+
+# In[90]:
+
+
+def create_class_visualization(target_y, model, dtype, **kwargs):
+    """
+    Generate an image to maximize the score of target_y under a pretrained model.
+    
+    Inputs:
+    - target_y: Integer in the range [0, 1000) giving the index of the class
+    - model: A pretrained CNN that will be used to generate the image
+    - dtype: Torch datatype to use for computations
+    
+    Keyword arguments:
+    - l2_reg: Strength of L2 regularization on the image
+    - learning_rate: How big of a step to take
+    - num_iterations: How many iterations to use
+    - blur_every: How often to blur the image as an implicit regularizer
+    - max_jitter: How much to gjitter the image as an implicit regularizer
+    - show_every: How often to show the intermediate result
+    """
+    model.type(dtype)
+    l2_reg = kwargs.pop('l2_reg', 1e-3)
+    learning_rate = kwargs.pop('learning_rate', 25)
+    num_iterations = kwargs.pop('num_iterations', 1000)
+    blur_every = kwargs.pop('blur_every', 10)
+    max_jitter = kwargs.pop('max_jitter', 16)
+    show_every = kwargs.pop('show_every', 25)
+
+    # Randomly initialize the image as a PyTorch Tensor, and also wrap it in
+    # a PyTorch Variable.
+    img = torch.randn(1, 3, 224, 224).mul_(1.0).type(dtype)
+    img_var = Variable(img, requires_grad=True)
+    for t in range(num_iterations):
+        # Randomly jitter the image a bit; this gives slightly nicer results
+        ox, oy = random.randint(0, max_jitter), random.randint(0, max_jitter)
+        img.copy_(jitter(img, ox, oy))
+
+        ########################################################################
+        # TODO: Use the model to compute the gradient of the score for the     #
+        # class target_y with respect to the pixels of the image, and make a   #
+        # gradient step on the image using the learning rate. Don't forget the #
+        # L2 regularization term!                                              #
+        # Be very careful about the signs of elements in your code.            #
+        ########################################################################
+        # Forward pass.
+        scores = model(img_var)
+        
+        # Score for the target class.
+        target_score = scores[0,target_y] 
+        
+        # Backward pass to get gradient wrt image.
+        target_score.backward()
+        
+        im_grad = img_var.grad.data
+        im_grad -= 2*l2_reg*img_var.data  # Add l2 reg derivative.
+                
+        # Update our image with normalised gradient (gives nicer images that unnormalised).
+        img_var.data += learning_rate * im_grad / im_grad.norm() 
+        
+        # Need to manually zero the image gradients.
+        img_var.grad.data.zero_()
+        ########################################################################
+        #                             END OF YOUR CODE                         #
+        ########################################################################
+        
+        # Undo the random jitter
+        img.copy_(jitter(img, -ox, -oy))
+
+        # As regularizer, clamp and periodically blur the image
+        for c in range(3):
+            lo = float(-SQUEEZENET_MEAN[c] / SQUEEZENET_STD[c])
+            hi = float((1.0 - SQUEEZENET_MEAN[c]) / SQUEEZENET_STD[c])
+            img[:, c].clamp_(min=lo, max=hi)
+        if t % blur_every == 0:
+            blur_image(img, sigma=0.5)
+        
+        class_names = ['NIH', 'CheXpert']
+        # Periodically show the image
+        if t == 0 or (t + 1) % show_every == 0 or t == num_iterations - 1:
+            plt.imshow(deprocess(img.clone().cpu()))
+            class_name = class_names[target_y]
+            plt.title('%s\nIteration %d / %d' % (class_name, t + 1, num_iterations))
+            plt.gcf().set_size_inches(4, 4)
+            plt.axis('off')
+            plt.show()
+
+    return deprocess(img.cpu())
+
+
+# Once you have completed the implementation in the cell above, run the following cell to generate an image of a Tarantula:
+
+# In[91]:
+
+
+#path_images = '/home/lovebb/Documents/MIBLab/chest-Xray-dataset'
+path_model = '/home/ben/Desktop/MIBLab/hospital-cls/reproduce-chexnet/results/checkpoint'
+
+checkpoint = torch.load(path_model, map_location=lambda storage, loc: storage)
+model = checkpoint['model']
+del checkpoint
+
+dtype = torch.FloatTensor
+# dtype = torch.cuda.FloatTensor # Uncomment this to use GPU
+model.type(dtype)
+
+target_y = 1 # Tarantula
+# target_y = 78 # Tick
+# target_y = 187 # Yorkshire Terrier
+# target_y = 683 # Oboe
+# target_y = 366 # Gorilla
+# target_y = 604 # Hourglass
+out = create_class_visualization(target_y, model, dtype)
+
+
+'''
+# Try out your class visualization on other classes! You should also feel free to play with various hyperparameters to try and improve the quality of the generated image, but this is not required.
+
+# In[87]:
+
+
+# target_y = 78 # Tick
+# target_y = 187 # Yorkshire Terrier
+# target_y = 683 # Oboe
+# target_y = 366 # Gorilla
+# target_y = 604 # Hourglass
+target_y = np.random.randint(1000)
+print(class_names[target_y])
+X = create_class_visualization(target_y, model, dtype)
+'''
